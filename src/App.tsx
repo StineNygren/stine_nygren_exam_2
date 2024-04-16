8773ced4bb4c7b2e72a570ee8773c3d7e7ca1215
StineNygren <107799331+StineNygren@users.noreply.github.com>
2024-04-16 11:17:11 +0200

    router is made, migth have to add private route later

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,13 @@
-
-
-import { Button } from '@mui/material';
-import { useGetVenuesQuery } from './services/api.reducer'
-
-
+import { RouterProvider } from 'react-router-dom'
+import Router from './components/Router';
 
 function App() {
-  const { data, error, isLoading } = useGetVenuesQuery()
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
 
-  if (error) {
-    return <div>Error</div>;
-  }
 
   return (
-    <div>
-      <h1>Venues</h1>
-      <ul>
-        {data?.map((venue) => (
-          <li key={venue.id}>
-            {venue.name} - {venue.description}
-          </li>
-        ))}
-        <Button variant="contained" color="primary">Primary</Button>
-      </ul>
+    <RouterProvider router={Router} />
 
-    </div>
   );
 }
 
