3b7254e27220fb2b4f72a524eddf2f9ae4cc5f0b
StineNygren <107799331+StineNygren@users.noreply.github.com>
2024-05-02 10:04:51 +0200

    made simple footer

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -1,8 +1,20 @@
+import { Box, Typography } from '@mui/material';
+import sunicon from '../../../assets/sunicon.png';
+
 function Footer() {
     return ( 
-        <footer>
-            <p>Footer</p>
-        </footer>
+        <Box sx={{backgroundColor: "#343434",  width: "100%"}} p={5}  display={"flex"} flexDirection={"column"} alignItems={"center"}>
+            <Box        sx={{
+            backgroundImage: `url(${sunicon})`,
+            height: '40px',
+            width: '60px',
+            backgroundSize: 'contain',
+            backgroundRepeat: 'no-repeat',
+
+        }}></Box>
+            <Typography fontSize={25} color={"primary"}>Holidaze</Typography>
+            <Typography  color={"primary"}>All rigths reserved - 2024</Typography>
+        </Box>
      );
 }
 
